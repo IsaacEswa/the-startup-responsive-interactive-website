f924995b83fa5db40dbb73ad55e88c7e4ba30d5f
IsaacEswa <isaaceswa@gmail.com>
2026-01-15 14:01:31 +0100

    hide programs op alle zenders

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,17 +19,55 @@ if (liveTime) {
 
 // Stap 1: selecteer Veronica radio button
 let veronicaRadioButton = document.querySelector('.veronica-button');
-// selecteer alle programma's met data-category "veronica"
+// selecteer alle articles met class "veronica"
 let veronicaPrograms = document.querySelectorAll('article.veronica');
 
 // Stap 2: voeg click event listener toe aan de button met een callback functie
-veronicaRadioButton.addEventListener('click', hidePrograms)
+veronicaRadioButton.addEventListener('click', hideVeronicaPrograms)
 
 // Stap 3: definieer de callback functie
-function hidePrograms() {
+function hideVeronicaPrograms() {
     veronicaPrograms.forEach(article => {
         article.classList.toggle('hide-programs');
     });
 
     veronicaRadioButton.classList.toggle('active-radiobutton');
+}
+
+
+
+// Stap 1: selecteer SLAM radio button
+let slamRadioButton = document.querySelector('.slam-button');
+// selecteer alle articles met class "slam"
+let slamPrograms = document.querySelectorAll('article.slam');
+
+// Stap 2: voeg click event listener toe aan de button met een callback functie
+slamRadioButton.addEventListener('click', hideSlamPrograms)
+
+// Stap 3: definieer de callback functie
+function hideSlamPrograms() {
+    slamPrograms.forEach(article => {
+        article.classList.toggle('hide-programs');
+    });
+
+    slamRadioButton.classList.toggle('active-radiobutton');
+}
+
+
+
+// Stap 1: selecteer 100%NL radio button
+let honderdnlRadioButton = document.querySelector('.honderdnl-button');
+// selecteer alle articles met class "honderdnl"
+let honderdnlPrograms = document.querySelectorAll('article.honderdnl');
+
+// Stap 2: voeg click event listener toe aan de button met een callback functie
+honderdnlRadioButton.addEventListener('click', hideHonderdnlPrograms)
+
+// Stap 3: definieer de callback functie
+function hideHonderdnlPrograms() {
+    honderdnlPrograms.forEach(article => {
+        article.classList.toggle('hide-programs');
+    });
+
+    honderdnlRadioButton.classList.toggle('active-radiobutton');
 }
