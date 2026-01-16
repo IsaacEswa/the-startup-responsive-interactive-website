5e7e18a38a492786acb19ed1470a1d0e51d4f7ea
IsaacEswa <isaaceswa@gmail.com>
2026-01-16 11:17:50 +0100

    beginnetje hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -71,3 +71,27 @@ function hideHonderdnlPrograms() {
 
     honderdnlRadioButton.classList.toggle('active-radiobutton');
 }
+
+
+
+
+
+
+/****************************************/
+/* menu openen en sluiten met de button */
+/****************************************/
+
+/* JOUW CODE HIER - stap 4 */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+const menuButton = document.querySelector("button");
+const deNav = document.querySelector("nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+menuButton.addEventListener("click", toggleMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+
+function toggleMenu() {
+    deNav.classList.toggle("is-open");
+}
